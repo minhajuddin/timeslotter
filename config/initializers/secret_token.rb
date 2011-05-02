a9f6f2ad9670552fe802530ad42c1a92adca2ee5
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-05-02 21:07:06 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeslotter::Application.config.secret_token = 'bdaaee36674696a4f9fcaeb0ae3cab65e611029385e7a444a43962c1b5cfcaac01c07b3e578356038cfe56ba9b509d8818b20a67055f69ef14f8b2f2370fa2c6'
